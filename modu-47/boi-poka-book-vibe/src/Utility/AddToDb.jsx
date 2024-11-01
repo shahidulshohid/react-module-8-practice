4861af49a9d3a46c9f266cda8e287d002495bfc7
shahidul shohid <shahidulislamshohid7gmail.com>
2024-11-01 13:14:26 +0600

    module 47 practice is something done

---
+++ modu-47/boi-poka-book-vibe/src/Utility/AddToDb.jsx
@@ -0,0 +1,49 @@
+const getStoredReadList = ()=> {
+    //read list
+    const storedListStr = localStorage.getItem('read-list')
+    if(storedListStr){
+        const storedList = JSON.parse(storedListStr)
+        return storedList
+    }
+    else {
+        return []
+    }
+}
+
+const addToStoredReadList = (id) => {
+    const storedList = getStoredReadList()
+
+    if(storedList.includes(id)){
+        //already exists. don't add it 
+        console.log(id, 'already exists in the read list') 
+    }
+    else{
+        storedList.push(id)
+        const storedListStr = JSON.stringify(storedList)
+        localStorage.setItem('read-list', storedListStr)
+
+    }
+}
+// for wishlist 
+const getStoredWishList = ()=>{
+    const storedListStr = localStorage.getItem('wish-list')
+    if(storedListStr){
+        const storedList = JSON.parse(storedListStr)
+        return storedList
+    }
+    return []
+}
+const addToStoredWishList = (id)=> {
+    const storedList = getStoredWishList()
+    if(storedList.includes(id)){
+        console.log(id, 'already exist in the wish list')
+    }
+    else{
+        storedList.push(id)
+        const storedListStr = JSON.stringify(storedList)
+        localStorage.setItem('wish-list', storedListStr)
+    }
+}
+
+export{addToStoredReadList, addToStoredWishList}
+
--- modu-47/boi-poka-book-vibe/src/components/BookDetail/BookDetail.jsx
+++ modu-47/boi-poka-book-vibe/src/components/BookDetail/BookDetail.jsx
@@ -1,12 +1,36 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList, addToStoredWishList } from "../../Utility/AddToDB";
 
 const BookDetail = () => {
     const {bookId} = useParams()
-
-    console.log(bookId)
+    const data = useLoaderData()
+    const id = parseInt(bookId)
+    
+    const book = data.find(book => book.bookId === id)
+    console.log(book)
+    const {bookId: currentBookId, image} = book
+    // for read list
+    const handleMarkAsRead = (id) => {
+        /**
+         * 1. understand what to store or save: => bookId
+         * 2. where to store: database
+         * 4. array, list, collection
+         * check: if the book is already in read list
+         * if not, then add the book to the list 
+         * if yes, don't add the book
+         */
+        addToStoredReadList(id)
+    }
+    // for wish list 
+    const handleWishList = (id) => {
+        addToStoredWishList(id)
+    }
     return (
         <div>
             <h2>Book Details: {bookId}</h2>
+            <img className="w-36 my-6" src={image} alt="" />
+            <button onClick={()=>handleMarkAsRead(currentBookId)} className=" btn-outline mr-4 btn btn-accent">Mark as Read</button>
+            <button onClick={()=>handleWishList(currentBookId)} className="btn btn-accent">Add to Wish List</button>
         </div>
     );
 };
